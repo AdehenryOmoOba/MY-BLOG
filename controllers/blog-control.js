821e1a7a674192ed6dc7fc3448afa3bb55e57ed1
Adefemi Adelaja <adefemiadelaja@gmail.com>
2021-11-03 15:29:45 +0100

    set up

--- controllers/blog-control.js
+++ controllers/blog-control.js
@@ -4,12 +4,46 @@ const BlogModel = require('../models/blog-model')
 
 
 // Routes
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+     
+    const allBlogs = await BlogModel.find({});
+    
+    res.render('index', {blogs: allBlogs})
 })
 
 router.get('/compose', (req, res) => {
+
+    
+
     res.render('composeBlog')
+})
+
+router.post('/compose', async (req, res) => {
+    const {title, content} = req.body;
+    console.log(title | content);
+
+    try {
+        if (!title || !content){
+            res.send('Please provide Title and/or Content...')
+        }
+
+        const newBlog = new BlogModel({title, content})
+
+        newBlog.save();
+
+        res.redirect('/')
+
+        console.log('Blog Posted Successfully')
+
+    } catch (error) {
+        console.log(error)
+        res.render('composeBlog')
+    }
+
+    
+
+
+  
 })
 
 
