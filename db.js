c65e9a0cd8a5ab649655876564729dc87ef5dca4
Adefemi Adelaja <adefemiadelaja@gmail.com>
2021-11-08 15:22:03 +0100

    admin login page responsiveness

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const uri =  process.env.DB_URI
+const uri =  process.env.DB_URI;
 
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
 
 if (err) {
-    console.log('Connection to database fail')
+    console.log('Connection to database fail' + err)
 } else{
     console.log('Connection to database successful')
 }
